e50a98eb3a829200fc2c9603f19b25d20f5198b1
thdudws <139088254+thdudws@users.noreply.github.com>
2023-09-21 17:36:42 +0900

    23.09.21

--- js/sub.js
+++ js/sub.js
@@ -16,11 +16,50 @@ $(function(){
         $('.ls_box>p').html(data);
     });
 
-
-    $('.toggle').click(function(){
-        $('.tg').toggle();
-        $('.btn1').css('display','none');
+    $.get("./sub_txt/text5.txt", function(data){
+        $('.click>p').html(data);
     });
+
+
+
+        $('.artwrap').on("click", function(){
+            $('.tg').toggleClass('hid');
+            if($('.btn1 span').text()=='펼쳐보기'){
+                $('.btn1 span').text('접기');
+            }else {
+                $('.btn1 span').text('펼쳐보기');
+            }
+
+            $('.btn1 button').toggleClass('rotate')
+            
+        });
+
+
+        $('.rw_wrap').on("click", function(){
+            $('.toggle2').toggleClass('hid2');
+            if($('.btn2 span').text()=='펼쳐보기'){
+                $('.btn2 span').text('접기');
+            }else {
+                $('.btn2 span').text('펼쳐보기');
+            }
+
+            $('.btn2 button').toggleClass('rotate')
+            
+        });
+
+
+        $('.wr').on("click", function(){
+            $('.click').toggleClass('none');
+        });
+
+    
+        $('.starz').mouseover(function(){
+            $('.starz>img').css('transform','translateY(-45px)')
+        });
+
+        $('.starz').mouseout(function(){
+            $('.starz>img').css('transform','translateY(0px)')
+        });
 
    
 });
